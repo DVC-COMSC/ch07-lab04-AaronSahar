d310fdbc95078cd7ea14089dc433079ab563e91e
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 16:56:29 -0700

    test

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-
-# ******************************
-# Make your Code
-# ******************************
 import random
 
 numbers1 = []
 numbers2 = []
 result = []
 for i in range(10):
-	numbers1.append(random.randint(0,100))
-	numbers2.append(random.randint(0,100))
+	numbers1.append(random.randint(0,10))
+	numbers2.append(random.randint(0,10))
+	result.append(numbers1[i] + numbers2[i])
 print (numbers1)
 print (numbers2)
+print (result)
